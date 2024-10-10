ceb13fa06cde04e4edf7160bb0199afd9bf73db5
Arsenii Yurchenko <arseniiyurchenko@Arseniis-MacBook-Air.local>
2024-10-10 16:59:17 +0300

    Setup pre commit hook

--- nuredu-api/src/courses/courses.controller.ts
+++ nuredu-api/src/courses/courses.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from "@nestjs/common";
+import { Controller, Get, Post, Patch, Param, Delete } from "@nestjs/common";
 import { CoursesService } from "./courses.service";
 
 @Controller("courses")
